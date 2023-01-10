0538c1d0f4ebda8fc65bf1c0a391f672d1908e45
voidKandy <121535853+voidKandy@users.noreply.github.com>
2023-01-10 09:20:49 -0800

    Jan 10

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ import gradio as gr
 learn_inf = load_learner('export.pkl')
 categories = ('radiohead', 'voidkandy', 'king_gizzard')
 def classify_specs(img):
-    img = img.reshape((-1, 256, 256, 3))
     pred, idx, probs = learn_inf.predict(img)
     return dict(zip(categories, map(float,probs)))
-
 image = gr.inputs.Image(shape=(192,192))
 label = gr.outputs.Label()
 
